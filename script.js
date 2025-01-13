c7d9f6da7c12a0f3ec68e7dc94c7d22ec6382085
omar <omarhrn@gmail.com>
2025-01-13 17:40:12 -0300

    Add todo

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
-const todoInput = document.querySelector('.project-input');
+const projectInput = document.querySelector('.project-input');
 const addButton = document.getElementById('add-button');
+const todoInput = document.querySelector('.todo-input');
+const addButtonTodo = document.getElementById('add-button-todo');
 const projects = [];
+const todos = [];
 
-function Projects (title, todos) {
+function Projects (title) {
   this.title = title;
-  this.todos = todos;
 }
 
-function Todo( title, description, dueDate, priority, notes, completed, project,
-) {
+function Todo( title ) {
   this.title = title;
-  this.description = description;
-  this.dueDate = dueDate;
-  this.priority = priority;
-  this.notes = notes;
-  this.completed = completed;
-  this.project = project;
 }
 
 function addProject() {
-  const projectTitle = todoInput.value.trim();
+  const projectTitle = projectInput.value.trim();
 
   if( projectTitle === '' ) return;
 
-  const project = new Projects(projectTitle, []);
+  const project = new Projects(projectTitle);
   projects.push(project);
 
   const container = document.getElementById('container');
@@ -31,16 +26,40 @@ function addProject() {
   card.className = 'projects-card';
   card.innerHTML = `
     <h2>${project.title}</h2>
+    <span class="todo-container"></span>
     <input type="text" placeholder="Add a new todo..." class="todo-input">
+    <button id="add-button-todo">Add</button>
   `
 
   container.appendChild(card);
-  todoInput.value = '';
+  projectInput.value = '';
 }
 
 addButton.addEventListener('click', addProject);
-todoInput.addEventListener('keypress', (event) => {
+projectInput.addEventListener('keypress', (event) => {
   if (event.key === 'Enter') {
     addProject();
   }
-})
+});
+
+function addTodo() {
+  const todoTitle = todoInput.value.trim();
+
+  if (todoTitle === '') return;
+
+  const todo = new Todo(todoTitle);
+  todos.push(todo);
+
+  const todoContainer = document.querySelector('.todo-container');
+  const todoCard = document.createElement('div');
+  todoCard.className = 'todo-card';
+  todoCard.innerHTML = `
+    <h3>${todo.title}</h3>
+  `
+  todoContainer.appendChild(todoCard);
+
+  todoInput.value = '';
+
+}
+
+addButtonTodo.addEventListener('click', addTodo);
